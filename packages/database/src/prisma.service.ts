644044061f325b1c7ea3ad2a88c08a2968dcadd4
halomanlodestar <kunalranarj2005@gmail.com>
2025-02-22 16:48:53 +0530

    Successfully extracted prisma into it's own package

--- packages/database/src/prisma.service.ts
+++ packages/database/src/prisma.service.ts
@@ -1,17 +1,13 @@
 /** @format */
 
 import { Injectable, OnModuleDestroy, OnModuleInit } from "@nestjs/common";
-import { PrismaClient } from "@prisma/client"; // Updated import statement
+import { PrismaClient } from "../prisma/output"; // Updated import statement
 
 @Injectable()
 export class PrismaService
 	extends PrismaClient
 	implements OnModuleInit, OnModuleDestroy
 {
-	constructor() {
-		super(); // Ensure the PrismaClient is properly instantiated
-	}
-
 	async onModuleInit() {
 		await this.$connect();
 		console.log("Connected to the database");
